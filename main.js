9ad679e86f8b0a1e3cb901a9dacb5964fda72288
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-06-27 09:03:24 +0300

    adding notes audio

---
+++ main.js
@@ -0,0 +1,19 @@
+var context=new AudioContext();
+//   var o=null;
+//   var g=null;
+
+const music=document.getElementById("key")
+music.addEventListener("click",function playNote(frequency, type) {
+    console.log("hdhdsdsjdkjs")
+    // setTimeout(function(){
+    //   o = context.createOscillator();
+    //     g = context.createGain();
+    //     o.type = type;
+    //     o.connect(g);
+    //     o.frequency.value = frequency;
+    //     g.connect(context.destination);
+    //     o.start(0);
+    //     g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 1);
+    // },1000)
+      
+  })
